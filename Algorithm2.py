0f9e05f874579787a3d39576d13ad4e35dee8875
lukash-ST88 <lukash.andrei2012@yandex.ru>
2022-09-26 11:37:21 +0300

    Add a second algorithm

---
+++ Algorithm2.py
@@ -0,0 +1,14 @@
+num = 5
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    f = 1
+    i = 1
+    while i <= n:
+        f = f * i
+        i += 1
+    return f
+x = factorial(4)
+print(x)
